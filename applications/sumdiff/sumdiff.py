1ef857c969168e15943e47ecfc091c75f5e3b020
CyrusTheCurator <joshua.c.luscombe@gmail.com>
2020-09-17 15:04:38 -0700

    fix solution

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -16,9 +16,9 @@ sum_storage = {}
 diff_storage = {}
 for x in q:
     for y in q:
-        sum_storage[(x,y)] = f(x) + f(y)
+        sum_storage[(x,y)] = (f(x), f(y))
         
-        diff_storage[(x,y)] = f(x) - f(y)
+        diff_storage[(x,y)] = (f(x), f(y))
 
 print(diff_storage)
 #get sum_key from sum_dict
@@ -29,10 +29,15 @@ print(diff_storage)
 #diff_key = f(c)-f(d) that is equal to f(a)+f(b)
 for sum_tuple in sum_storage:
     for diff_tuple in diff_storage:
+        a = sum_storage[sum_tuple][0]
+        b = sum_storage[sum_tuple][1]
+        c = diff_storage[diff_tuple][0]
+        d = diff_storage[diff_tuple][1]
 
-        if sum_storage[sum_tuple] == diff_storage[diff_tuple]:
 
-            print(f"{f(sum_tuple[0])} + {f(sum_tuple[1])} = {f(diff_tuple[0])} - {f(diff_tuple[1])}")
+
+        if a + b == c - d:
+            print(f"{a} + {b} = {c} - {d}")
 
 
 
